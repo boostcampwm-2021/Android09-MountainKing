629c725c486d2995a1f2c17fcc851f1a0720b1b9
Frogrice <puhahaha3000@naver.com>
2021-11-04 15:29:48 +0900

    feat(achievement): progressAchievement 메서드 추가

--- app/src/main/java/com/boostcamp/mountainking/entity/Achievement.kt
+++ app/src/main/java/com/boostcamp/mountainking/entity/Achievement.kt
@@ -2,6 +2,7 @@ package com.boostcamp.mountainking.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.boostcamp.mountainking.data.Statistics
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -23,4 +24,26 @@ data class Achievement(
 ) {
     private val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale("ko", "KR"))
     val completeDateString: String get() = completeDate?.let { dateFormat.format(it) } ?: ""
+
+    fun progressAchievement(statistics: Statistics) {
+        curProgress = when (type) {
+            AchievementType.TRACKING_TOTAL_DISTANCE -> {
+                statistics.distance
+            }
+            AchievementType.TRACKING_TOTAL_COUNT -> {
+                statistics.mountainMap.values.sum()
+            }
+            AchievementType.TRACKING_PERIOD_COUNT -> TODO()
+            AchievementType.MOUNTAIN_COUNT -> {
+                statistics.mountainMap[typeArgument]?:0
+            }
+            AchievementType.MOUNTAIN_KIND_COUNT -> {
+                statistics.mountainMap.filter { entry ->
+                    entry.value != 0
+                }.filter { entry ->
+                    (typeArgument as List<Int>).contains(entry.key)
+                }.size
+            }
+        }
+    }
 }
