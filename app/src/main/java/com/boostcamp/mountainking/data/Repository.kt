3c1bedc93e6b27ed44072afe500e7b319bfd2034
dokyung <dokyung.dev@gmail.com>
2021-11-04 18:50:47 +0900

    fix(achievement): database 버그 수정

--- app/src/main/java/com/boostcamp/mountainking/data/Repository.kt
+++ app/src/main/java/com/boostcamp/mountainking/data/Repository.kt
@@ -6,9 +6,9 @@ import com.boostcamp.mountainking.entity.Achievement
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class Repository(private val context: Context) : RepositoryInterface {
+class Repository(context: Context) : RepositoryInterface {
 
-    val database = AchievementDatabase.provideAchievementDao(context)
+    val database = AppDatabase.getInstance(context)?.achievementDao()
 
     override suspend fun getMountain() {
         //TODO("산정보불러오기")
@@ -18,21 +18,15 @@ class Repository(private val context: Context) : RepositoryInterface {
         //TODO("등산기록불러오기")
     }
 
-    override fun getAchievement(): List<Achievement> {
-
-        //Log.d("count", database.countData().toString())
-
-        getInitAchievementList().forEach {
-            //database.insert(it)
-            Log.d("achievement", it.name)
+    override suspend fun getAchievement(): List<Achievement> = withContext(Dispatchers.IO) {
+        if(database?.countData() == 0){
+            getInitAchievementList().forEach {
+                database.insert(it)
+            }
         }
-//                val list = database.getAchievementData()
-//                list.forEach {
-//                    Log.d("db", it.name)
-//                }
-//                list
-        return getInitAchievementList()
+        database?.getAchievementData() ?: listOf()
     }
+
 
     override suspend fun getStatistics() {
         //TODO("통계불러오기")
@@ -50,7 +44,7 @@ class Repository(private val context: Context) : RepositoryInterface {
         //TODO("통계최신화하기")
     }
 
-    override suspend fun updateAchievement() {
-        //TODO("업적최신화하기")
+    override suspend fun updateAchievement(achievement: Achievement) {
+        database?.updateAchievement(achievement)
     }
 }
