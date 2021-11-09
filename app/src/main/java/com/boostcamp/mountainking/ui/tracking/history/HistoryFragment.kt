71ebb4fb708e61a1352866724430ff9884408c1b
JongDo-android <whdgus090@gmail.com>
2021-11-09 14:26:10 +0900

    feat(History): HistoryFragment, ViewModel 생성

---
+++ app/src/main/java/com/boostcamp/mountainking/ui/tracking/history/HistoryFragment.kt
@@ -0,0 +1,63 @@
+package com.boostcamp.mountainking.ui.tracking.history
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.boostcamp.mountainking.R
+import com.boostcamp.mountainking.databinding.FragmentHistoryBinding
+import com.boostcamp.mountainking.ui.tracking.history.adapter.HistoryAdapter
+
+class HistoryFragment
+    : Fragment(), OnHistoryItemClickListener {
+    private lateinit var binding: FragmentHistoryBinding
+    private val historyViewModel: HistoryViewModel by viewModels()
+    private val historyAdapter by lazy {
+        HistoryAdapter(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolbar()
+        initView()
+    }
+
+    private fun initToolbar() {
+        with(activity as AppCompatActivity) {
+            setSupportActionBar(binding.tbHistory)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_new_24)
+        }
+
+        binding.tbHistory.setNavigationOnClickListener {
+            findNavController().navigate(R.id.action_history_to_tracking)
+        }
+    }
+
+    private fun initView() {
+        with(binding) {
+            rvHistoryList.adapter = historyAdapter
+        }
+    }
+
+    override fun onItemClick() {
+        // TODO History item click action
+    }
+
+    companion object {
+        private const val TAG = "HistoryFragment"
+    }
+}
