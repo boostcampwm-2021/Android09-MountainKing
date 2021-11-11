25367cda1be170d463f1bdb2de80784e685aaee2
JongDo-android <whdgus090@gmail.com>
2021-11-11 15:23:45 +0900

    feat(LatLngAlt): Parcelize annotation 추가

--- app/src/main/java/com/boostcamp/mountainking/data/LatLngAlt.kt
+++ app/src/main/java/com/boostcamp/mountainking/data/LatLngAlt.kt
@@ -1,8 +1,13 @@
 package com.boostcamp.mountainking.data
 
 import android.location.Location
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-data class LatLngAlt(val latitude: Double, val longitude: Double, val altitude: Double) {
+
+@Parcelize
+data class LatLngAlt(val latitude: Double, val longitude: Double, val altitude: Double)
+    : Parcelable {
     companion object {
         fun fromLocation(location: Location): LatLngAlt {
             return LatLngAlt(location.latitude, location.longitude, location.altitude)
