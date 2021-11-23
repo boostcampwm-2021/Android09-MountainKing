f9145a03c4f9d418cfe764e090e3d366dc997232
JongDo-android <whdgus090@gmail.com>
2021-11-23 14:19:31 +0900

    feat(Entity): Tracking Parcelize annotation 추가

--- app/src/main/java/com/boostcamp/mountainking/entity/Tracking.kt
+++ app/src/main/java/com/boostcamp/mountainking/entity/Tracking.kt
@@ -1,11 +1,15 @@
 package com.boostcamp.mountainking.entity
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.boostcamp.mountainking.data.LatLngAlt
+import kotlinx.parcelize.Parcelize
+
 
 @Entity
-data class Tracking(
+@Parcelize
+data class Tracking (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val mountainName: String,
@@ -13,4 +17,4 @@ data class Tracking(
     val coordinates: List<LatLngAlt>,
     val duration: String,
     val distance: String?
-)
+) : Parcelable
